c6973869f634cf239c7e56e3a8ce0ee08ff12301
ReFantasy <309741309@qq.com>
2019-08-13 21:16:35 +0800

    添加一维傅里叶及其逆变换

--- Fourier.cpp
+++ Fourier.cpp
@@ -1,5 +1,27 @@
 ﻿#include "Fourier.h"
 
+Eulerform::Eulerform(const std::complex<double> &com)
+	:_com(com)
+{
+
+}
+
+Eulerform::Eulerform(double mul, double thet)
+{
+	double re = mul * cos(thet);
+	double im = mul * sin(thet);
+	_com = std::complex<double>(re, im);
+}
+
+double Eulerform::Mul() const
+{
+	return std::abs(_com);
+}
+
+double Eulerform::Thet() const
+{
+	return std::arg(_com);
+}
 
 
 Fourier::Fourier()
@@ -10,3 +32,62 @@ Fourier::Fourier()
 Fourier::~Fourier()
 {
 }
+
+const std::complex<double>& Eulerform::GetComplex() const
+{
+	return _com;
+}
+
+
+
+std::vector<std::complex<double>> Fourier::fourier(const std::vector<double> &array_in) const
+{
+	int count = array_in.size();
+	std::vector<std::complex<double>> array_out(count, std::complex<double>{});
+
+	for (int i = 0; i < count; i++)
+	{
+		std::complex<double> tmp;
+		for (int j = 0; j < count; j++)
+		{
+			Eulerform el(1, -2.0*PI*i*j / count);
+			tmp += (array_in[j] * el.GetComplex());
+		}
+		array_out[i] = tmp;
+	}
+
+	return array_out;
+}
+
+std::vector<double> Fourier::InversionFourier(const std::vector<std::complex<double>> &array_in, std::vector<std::complex<double>> *pout_media)
+{
+	int M = array_in.size();
+	std::vector<double> array_out(M, 0);
+	std::vector<std::complex<double>> array_out_com(M, std::complex<double>{});
+
+	for (int i = 0; i < M; i++)
+	{
+		std::complex<double> tmp;
+		for (int j = 0; j < M; j++)
+		{
+			Eulerform el(1, 2.0*PI*i*j / M);
+			tmp += (array_in[j] * el.GetComplex());
+		}
+		array_out_com[i] = { tmp.real() / M, tmp.imag() / M };
+	}
+
+
+	if (pout_media)
+	{
+		*pout_media = array_out_com;
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		array_out[i] = array_out_com[i].real();
+	}
+
+	return array_out;
+}
+
+
--- Fourier.h
+++ Fourier.h
@@ -1,7 +1,29 @@
 ﻿#ifndef __FOURIER_H__
 #define __FOURIER_H__
+
 #include <complex>
 #include <vector>
+
+#ifdef _DEBUG
+#include <iostream>
+#endif
+
+#define PI 3.141592653
+
+// 欧拉公式
+// thet 单位为弧度
+class Eulerform
+{
+public:
+	Eulerform(const std::complex<double> &com);
+	Eulerform(double mul, double thet);
+	double Mul()const;
+	double Thet()const;
+	const std::complex<double>& GetComplex()const;
+
+private:
+	std::complex<double> _com;
+};
 
 class Fourier
 {
@@ -9,8 +31,21 @@ public:
 	Fourier();
 	~Fourier();
 
-	// 一维傅里叶变换
+	// 功能：一维傅里叶变换
+	// 输入：一维浮点数据向量
+	// 输出：一维复数向量
+	std::vector<std::complex<double>> fourier(const std::vector<double> &array_in)const;
+
+
+	 // 功能 ：一维傅里叶逆变换
+	 // 参数1：需要进行逆变换的一维复数向量
+	 // 参数2：一维复数向量的指针，用于返回逆变换的中间计算结果，如果不需要可以传递空参数
+	 // 输出 ：一维浮点数据向量，即傅里叶逆变换的结果
+
+	std::vector<double> InversionFourier(const std::vector<std::complex<double>> &array_in, std::vector<std::complex<double>> *pout_media = nullptr);
 };
+
+
 
 #endif//__FOURIER_H__
 
