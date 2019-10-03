385c2922faf9daa9243835a260ad4f536f8b9c4c
ReFantasy <309741309@qq.com>
2019-10-03 18:39:46 +0800

    update

---
+++ sample/ch09/P429F9_35.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>      
+#include "opencv2/opencv.hpp"
+#include "utility/utility.hpp"
+#include "algorithm/algorithm.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char*argv[])
+{
+	Mat src = imread("../data/DIP3E_Original_Images_CH09/Fig0935(a)(ckt_board_section).tif", 0);
+	
+	Mat se = getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5,5));
+
+	Mat b;
+	erode(src, b, se);
+
+	Mat c;
+	dilate(src, c, se);
+
+	imshow("a", src);
+	imshow("b", b);
+	imshow("c", c);
+	
+	
+	waitKey();
+	return 0;
+}
+
+
+
---
+++ sample/ch09/P433F9_40.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>      
+#include "opencv2/opencv.hpp"
+#include "utility/utility.hpp"
+#include "algorithm/algorithm.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char*argv[])
+{
+	Mat src = imread("../data/DIP3E_Original_Images_CH09/Fig0940(a)(rice_image_with_intensity_gradient).tif", 0);
+	
+	Mat otsu;
+	threshold(src, otsu, 0, 255, THRESH_BINARY + THRESH_OTSU);
+	
+	Mat c;
+	Mat se = getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(81, 81));
+	morphologyEx(src, c, MORPH_OPEN, se);
+
+	Mat top_hat = src - c;
+
+	Mat e;
+	threshold(top_hat, e, 0, 255, THRESH_BINARY + THRESH_OTSU);
+
+	imshow("a", src);
+	imshow("b", otsu);
+	imshow("c", c);
+	imshow("d", top_hat);
+	imshow("e", e);
+	
+	
+	waitKey();
+	return 0;
+}
+
+
+
