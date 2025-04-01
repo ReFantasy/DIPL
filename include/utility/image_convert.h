bcd37a4de7d24f3830284c29560408e2283a493f
refantasy <refantasy@refantasydeMac-mini.local>
2025-04-01 10:35:45 +0800

    update

--- include/utility/image_convert.h
+++ include/utility/image_convert.h
@@ -16,6 +16,10 @@ namespace IPL
      */
     std::vector<std::vector<double>> Mat2Vector2D(const cv::Mat& mat);
 
+    /** \brief std::vector<std::vector<double>> to Mat
+     *  \param[in] std::vector<std::vector<double>>
+     *  \param[out] Mat CV_8UC1
+     */
     cv::Mat Vector2D2Mat(std::vector<std::vector<double>> src);
 
     /** \brief Mat to std::vector<std::vector<double>> g(x,y) = f(x,y)*(-1)^(x+y)
