fe5aeb4d19cc3ed8219da1cc2dcbb1fd269d3e6b
refantasy <refantasy@refantasydeMac-mini.local>
2025-04-05 17:32:38 +0800

    updte

--- include/utility/Timer.h
+++ include/utility/Timer.h
@@ -10,17 +10,36 @@ public:
     Timer() : _is_stoped(true) {}
     void Start() { _start_time = high_resolution_clock::now(); }
 
-    void ReSet() { _start_time = high_resolution_clock::now(); }
+    void ReSet()
+    {
+        suspends.clear();
+        _start_time = high_resolution_clock::now();
+    }
+
+    void Suspend() { _suspend_time = high_resolution_clock::now(); }
+    void Continue()
+    {
+        auto current_time = high_resolution_clock::now();
+        suspends.push_back(std::make_pair(_suspend_time, current_time));
+    }
 
     template<typename T = milliseconds>
     int Elapse() const
     {
-        return duration_cast<T>(high_resolution_clock::now() - _start_time).count();
+        int total_suspend_time = 0;
+        for (auto& suspend : suspends)
+        {
+            total_suspend_time += duration_cast<T>(suspend.second - suspend.first).count();
+        }
+        return duration_cast<T>(high_resolution_clock::now() - _start_time).count() - total_suspend_time;
     }
     ~Timer() {}
 
 private:
     high_resolution_clock::time_point _start_time;
+    high_resolution_clock::time_point _suspend_time;
+
+    std::vector<std::pair<high_resolution_clock::time_point, high_resolution_clock::time_point>> suspends;
 
     bool _is_stoped;
 };
--- sample/ch04/fourier_test.cpp
+++ sample/ch04/fourier_test.cpp
@@ -4,6 +4,7 @@
 
 int main()
 {
+    Timer timer;
     std::cout << "一维傅立叶变换 例4.4" << std::endl;
     std::vector<double> input = {1, 2, 4, 4};
 
@@ -14,16 +15,23 @@ int main()
     }
 
     std::cout << std::endl << "DFT:" << std::endl;
+    timer.Start();
     auto out = IPL::Fourier::DFT(input);
+    timer.Suspend();
     for (auto e : out)
     {
         std::cout << e << std::endl;
     }
 
     std::cout << std::endl << "IDFT:" << std::endl;
+    timer.Continue();
     auto src = IPL::Fourier::IDFT(out);
+
+    auto comsume_time = timer.Elapse<std::chrono::microseconds>();
+
     for (auto e : src)
     {
         std::cout << e << std::endl;
     }
+    std::cout << "time:" << comsume_time << " microseconds" << std::endl;
 }
